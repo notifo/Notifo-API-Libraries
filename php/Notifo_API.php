d24be2d8106a1faa4fc1f57335036aea9657e6f1
jszczypk <janusz@fide.pl>
2010-09-20 18:30:57 -0700

    sendRequest and accordingly new methods subscribeUser and sendNotification returns array instead of JSON string
    changes to CURL options

--- php/Notifo_API.php
+++ php/Notifo_API.php
@@ -16,27 +16,27 @@ class Notifo_API {
     $this->apiSecret = $apiSecret;
   }
 
-  /*
+  /**
    * function: sendNotification
    * @param: $params - an associative array of parameters to send to the Notifo API.
-   *                   These can be any of the following:
-   *                   to, msg, label, title, uri
-   *                   See https://api.notifo.com/ for more information
+   * These can be any of the following:
+   * to, msg, label, title, uri
+   * See https://api.notifo.com/ for more information
    */
   function sendNotification($params) {
     $validFields = array('to', 'msg', 'label', 'title', 'uri');
-    $data = array_intersect_key($data, array_flip($validFields));
-    return $this->sendRequest('send_notification', $data)
+    $params = array_intersect_key($params, array_flip($validFields));
+    return $this->sendRequest('send_notification', $params);
   } /* end function sendNotification */
 
-  /*
+  /**
    * function: subscribeUser
    * @param: $username - the username to subscribe to your Notifo service
-   *                     See https://api.notifo.com/ for more information
+   * See https://api.notifo.com/ for more information
    */
   function subscribeUser($username) {
     return $this->sendRequest('subscribe_user', array('username' => $username));
-  } /* end function subscribUser */
+  } /* end function subscribeUser */
 
 
   /**
@@ -45,19 +45,18 @@ class Notifo_API {
    * @param $data - array with arguments for remote method
    */
   protected function sendRequest($method, $data) {
-
     $ch = curl_init(self::API_ROOT.self::API_VER.'/'.$method);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
     curl_setopt($ch, CURLOPT_USERPWD, $this->apiUsername.':'.$this->apiSecret);
     curl_setopt($ch, CURLOPT_HEADER, false);
-    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
     $result = curl_exec($ch);
-    return $result
+    $result = json_decode($result, true);
+    return $result;
   } /* end function sendRequest */
 
-  // for backwards compatibility
+// for backwards compatibility
   function send_notification($params) { return json_encode($this->sendNotification($params)); }
   function subscribe_user($username) { return json_encode($this->subscribeUser($username)); }
   
