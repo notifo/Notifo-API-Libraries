cdabf089c3eb059ba36949a309f12287bff8bb24
Notifo <notifo@notifo.com>
2010-09-21 03:12:05 +0000

    1. re-introduced setters for apiusername and apisecret in the cases where you want to send from multiple service/user accounts w/o having to create new objects 2. re-introduce the $type parameter for sendRequest to make it forward compatible with future API methods that may not be POST (and to be backward compatible) 3. reconfigured some CURL settings for forward compatible methods with non-POST type 4. un-protected the sendRequest method to expose the functionality in the case of new API methods published without an updated library

--- php/Notifo_API.php
+++ php/Notifo_API.php
@@ -16,6 +16,14 @@ class Notifo_API {
     $this->apiSecret = $apiSecret;
   }
 
+  function set_apiusername($val) {
+    $this->apiUsername = $val;
+  }
+
+  function set_apisecret($val) {
+    $this->apiSecret = $val;
+  }
+
   /**
    * function: sendNotification
    * @param: $params - an associative array of parameters to send to the Notifo API.
@@ -26,7 +34,7 @@ class Notifo_API {
   function sendNotification($params) {
     $validFields = array('to', 'msg', 'label', 'title', 'uri');
     $params = array_intersect_key($params, array_flip($validFields));
-    return $this->sendRequest('send_notification', $params);
+    return $this->sendRequest('send_notification', 'POST', $params);
   } /* end function sendNotification */
 
   /**
@@ -35,20 +43,27 @@ class Notifo_API {
    * See https://api.notifo.com/ for more information
    */
   function subscribeUser($username) {
-    return $this->sendRequest('subscribe_user', array('username' => $username));
+    return $this->sendRequest('subscribe_user', 'POST', array('username' => $username));
   } /* end function subscribeUser */
 
 
   /**
    * helper function to send the requests
    * @param $method - name of remote method to call
+   * @param $type - HTTP method (GET, POST, etc)
    * @param $data - array with arguments for remote method
    */
-  protected function sendRequest($method, $data) {
-    $ch = curl_init(self::API_ROOT.self::API_VER.'/'.$method);
+  function sendRequest($method, $type, $data) {
+
+    $url = self::API_ROOT.self::API_VER.'/'.$method;
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    if ($type == "POST") {
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    }
     curl_setopt($ch, CURLOPT_USERPWD, $this->apiUsername.':'.$this->apiSecret);
     curl_setopt($ch, CURLOPT_HEADER, false);
     $result = curl_exec($ch);
